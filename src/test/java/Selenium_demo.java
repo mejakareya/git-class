b95eb06344c2129f1fe9d132feb79a7ab78dfb26
jakareyame <107097830+jakareyame@users.noreply.github.com>
2024-03-17 12:29:03 +0600

    this class was all about checking css value and scrolling

--- src/test/java/Selenium_demo.java
+++ src/test/java/Selenium_demo.java
@@ -1,8 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -27,6 +30,7 @@ public class Selenium_demo {
         driver = new ChromeDriver(options);
         //driver.get("https://www.google.com/");
         driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+        driver.manage().window().maximize();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // before V4
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // after V4
         //250ms
@@ -46,13 +50,43 @@ public class Selenium_demo {
         //Thread.sleep(1000);
         //driver.findElement(By.xpath("//li[contains(text(),'Sign In Portal')]")).click();
         driver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
-        driver.findElement(By.id("usr")).sendKeys("Bug resistance");
+
+
+        /*driver.findElement(By.id("usr")).sendKeys("Bug resistance");
         driver.findElement(By.id("pwd")).sendKeys("testabc");
         driver.findElement(By.xpath("//input[@type='submit']")).click();
         String actualText = driver.findElement(By.xpath("//h3[contains(text(),'Formal Shoes')]")).getText();
         System.out.println(actualText);
         String expectedText = "Formal Shoes";
-        Assert.assertEquals(actualText,expectedText);
+        Assert.assertEquals(actualText,expectedText);*/
+
+        String actualBtnColor = driver.findElement(By.id("NewRegistration")).getCssValue("background-color");
+        System.out.println(actualBtnColor);
+        String hexValue = Color.fromString(actualBtnColor).asHex();
+        System.out.println(hexValue);
+
+        String expecetdColor = "#007bff";
+        Assert.assertEquals(hexValue,expecetdColor);
+
+        driver.findElement(By.id("NewRegistration")).click();
+        //Thread.sleep(2000);
+
+        driver.findElement(By.id("Salutation")).click();
+        //Thread.sleep(1000);
+        driver.findElement(By.xpath("//option[@value='Mrs']")).click();
+        //Thread.sleep(2000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,500)");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+        String actualTextSize = driver.findElement(By.xpath("//span[contains(text(),'This field is required')]")).getCssValue("font-size");
+        System.out.println(actualTextSize);
+
+        String expectedTextSize = "16px";
+
+        Assert.assertEquals(actualTextSize,expectedTextSize);
+
 
         // this is edited from firstbranch
     }
